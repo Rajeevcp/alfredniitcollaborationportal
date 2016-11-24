ea0b5d7c35c326055402f2c9895ef71956d9aeb4
alfredaugustin <alfredaugustin@hotmail.com>
2016-11-24 11:40:23 +0530

    bug fixes

---
+++ collabomatic/src/main/java/com/rest/springmvc/configuration/DBConfig.java
@@ -0,0 +1,51 @@
+package com.rest.springmvc.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.rest.springmvc.model.Person;
+import com.rest.springmvc.model.Test;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {    //creates an instance
+	@Bean
+	public SessionFactory sessionFactory() 
+	{
+		LocalSessionFactoryBuilder lsf=new LocalSessionFactoryBuilder(getDataSource());
+		Properties hibernateProperties=new Properties();
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.OracleDialect");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		lsf.addProperties(hibernateProperties);
+		return lsf.addAnnotatedClasses(Test.class).addAnnotatedClasses(Person.class)
+		   .buildSessionFactory();
+	}
+	
+	@Bean
+	public DataSource getDataSource() 
+	{
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+	    dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+	    dataSource.setUsername("collaboration");
+	    dataSource.setPassword("collabomatic");
+	    return dataSource;
+	}
+	
+	@Bean
+	public HibernateTransactionManager hibTransManagement()
+	{
+		return new HibernateTransactionManager(sessionFactory());
+	}
+}
