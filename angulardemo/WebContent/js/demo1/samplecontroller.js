1e844178993bd4fe16e80c6537b39dcf0092048d
alfredaugustin <alfredaugustin@hotmail.com>
2016-11-30 18:58:30 +0530

    AngularJS samples

---
+++ angulardemo/WebContent/js/demo1/samplecontroller.js
@@ -0,0 +1,8 @@
+var app=angular.module("personApp",[])
+app.controller('PersonController',function($scope){
+	$scope.message="Hello World"
+		
+    //var self=this;
+	// self.message="HelloWorld"
+	//self.person={id:1,name:'John',email:'John@xyz.com'}
+})
