34cce53a0582e6a4000091b8172589c81abaeada
alfredaugustin <alfredaugustin@hotmail.com>
2016-12-07 12:45:59 +0530

    The complete demo of the Collaboration project

--- angulardemo/WebContent/js/demo1/samplecontroller.js
+++ angulardemo/WebContent/js/demo1/samplecontroller.js
@@ -2,7 +2,21 @@ var app=angular.module("personApp",[])
 app.controller('PersonController',function($scope){
 	$scope.message="Hello World"
 		
-    //var self=this;
+    // var self=this;
 	// self.message="HelloWorld"
-	//self.person={id:1,name:'John',email:'John@xyz.com'}
+	// self.person={id:1,name:'John',email:'John@xyz.com'}
+	
+	$scope.username;
+	
+	$scope.login=function()
+	{
+		alert('logging in...')
+		$scope.username=true
+	}
+	
+	$scope.logout=function()
+	{
+		alert('logged out!')
+		$scope.username=false
+	}
 })
