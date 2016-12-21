d0d059dddb08154b555660a31179febded4d45c6
alfredaugustin <alfredaugustin@hotmail.com>
2016-12-21 23:27:50 +0530

    Completion of Login and Registration pages

---
+++ MAIN PROJECT/collbackend/src/main/java/com/niit/backend/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.niit.backend.dao;
+
+import com.niit.backend.model.User;
+
+public interface UserDao 
+{
+	User authenticate(User user);
+	void updateUser(User user);
+	User registerUser(User user);
+}
