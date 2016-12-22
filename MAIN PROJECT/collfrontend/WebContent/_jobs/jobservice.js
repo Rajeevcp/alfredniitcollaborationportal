47a3c4cd9b94330c43bbef56e3e099839807eadc
alfredaugustin <alfredaugustin@hotmail.com>
2016-12-22 23:23:35 +0530

    Implementation of Admin's role to manage wall posts

---
+++ MAIN PROJECT/collfrontend/WebContent/_jobs/jobservice.js
@@ -0,0 +1,18 @@
+app.factory('JobService',function($http)
+{
+	var jobService={};
+	
+	var BASE_URL ="http://localhost:8181/collbackend"
+	
+	jobService.saveJob=function(job)
+	{
+		return $http.post(BASE_URL + "/postJob" , job);
+	}
+	
+	jobService.getAllJobs=function()
+	{
+		return $http.get(BASE_URL + "/getAllJobs");
+	}
+	
+	return jobService;
+})
